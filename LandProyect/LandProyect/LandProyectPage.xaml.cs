dc83ae4e6d43465a82af455d58c256bd06583c49
Joel <Joelarias2508@gmail.com>
2018-04-01 13:31:50 -0400

    New proyect land

---
+++ LandProyect/LandProyect/LandProyectPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace LandProyect
+{
+    public partial class LandProyectPage : ContentPage
+    {
+        public LandProyectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
